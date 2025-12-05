6694c453f9fbb2aae6a7f3eba2af415c843b9961
Debjit Das <debjit826@gmail.com>
2025-12-05 23:57:48 +0530

    day 4 cleaned the code

--- src/noise.py
+++ src/noise.py
@@ -1,56 +1,5 @@
 import os
 from opensimplex import OpenSimplex
-import numpy as np
-
-# def generate_noise(width, height, seed=0, freq=0.3, octaves=1, lacunarity=2.0, persistence=0.5):
-#     gen = OpenSimplex(seed)
-#     def single_noise(x, y):
-#         return gen.noise2(x, y)
-#     out = np.zeros((height, width), dtype=np.float32)
-#     for y in range(height):
-#         for x in range(width):
-#             nx = x * freq
-#             ny = y * freq
-#             amp = 1.0
-#             freq_i = 1.0
-#             val = 0.0
-#             for o in range(octaves):
-#                 val += amp * single_noise(nx * freq_i, ny * freq_i)
-#                 amp *= persistence
-#                 freq_i *= lacunarity
-#             out[y, x] = val
-#     minv, maxv = out.min(), out.max()
-#     if maxv - minv == 0:
-#         normalized = np.zeros_like(out, dtype=np.float32)
-#     else:
-#         normalized = ((out - minv) / (maxv - minv)* 1.0).astype(np.float32)
-#     return normalized
-# 0.0 - 0.25 → deep water  
-# 0.25 - 0.35 → coast  
-# 0.35 - 0.55 → plains  
-# 0.55 - 0.75 → forest  
-# 0.75 - 1.0 → mountains
-# ~ → water  
-# . → plains  
-# T → forest  
-# ^ → mountains
-    
-# if __name__ == "__main__":
-#     w, h = 20, 20
-#     normalized = generate_noise(w, h)
-#     for row in normalized:
-#         for v in row:
-#             if v <=  0.25:
-#                 print("~", end="")
-#             elif v <= 0.35 and v > 0.25:
-#                 print(".", end="")
-#             elif v <= 0.55 and v > 0.35:
-#                 print(".", end="")
-#             elif v <= 0.75 and v > 0.55:
-#                 print("T", end="")
-#             else:
-#                 print("^", end="")
-#         print()
 
 width = 50
 height = 50
@@ -66,6 +15,7 @@ for y in range(height):
         row.append(value)
     grid.append(row)
 
+# normalize the values within 0 to 1 will be floats
 flat = [v for row in grid for v in row]
 low = min(flat)
 high = max(flat)
@@ -73,6 +23,7 @@ normal = []
 for row in grid:
     normal.append([(v-low) / (high-low) for v in row])
 
+# Creating the map
 def classify(value):
     if value < 0.25: return "water"
     if value < 0.45: return "sand"
@@ -99,8 +50,8 @@ for row in terrain:
 player = {
     "x": width // 2,
     "y": height // 2,
-    "symbol": "@"}
-view_width = 20
+    "symbol": "()"}
+view_width = 30
 view_height = 12
 
 def cam_movement():
