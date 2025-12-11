d0315db985f9c6ee3680039b4455ddb55c69350c
Debjit Das <debjit826@gmail.com>
2025-12-11 23:24:42 +0530

    day 10 implemented pixel art tiles, repositioned assests for better management

--- src/render.py
+++ src/render.py
@@ -10,12 +10,23 @@ class Renderer:
         pygame.font.init()
         self.width = self.tile_size * self.view_width
         self.height = self.tile_size * self.view_height
+        
         self.win = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Fractal Wilds")
         self.font = pygame.font.SysFont("rockwell", 20)
+        self.tile_images = {
+            "water": pygame.image.load("data/assets/water_pixel.png").convert_alpha(),
+            "sand": pygame.image.load("data/assets/sand_pixel.png").convert_alpha(),
+            "grass": pygame.image.load("data/assets/grass_pixel.png").convert_alpha(),
+            "forest": pygame.image.load("data/assets/forest_pixel.png").convert_alpha(),
+            "mountain": pygame.image.load("data/assets/mountain_pixel.png").convert_alpha(),
+        }
+        self.player_image = pygame.transform.scale(
+            pygame.image.load("data/assets/player_pixel.png").convert_alpha(), (self.tile_size, self.tile_size))
+        for key in self.tile_images:
+            self.tile_images[key]  = pygame.transform.scale(self.tile_images[key], (self.tile_size, self.tile_size))
 
     def draw(self, world, player):
-        #self.win.fill((0, 0, 0))
         cam_left = player["x"] - self.view_width //2
         cam_top = player["y"] - self.view_height // 2
 
@@ -32,12 +43,15 @@ class Renderer:
                 color = bio_[biome]["color"]
                 screen_x = col * self.tile_size
                 screen_y = row * self.tile_size
-                pygame.draw.rect(self.win, color, (screen_x, screen_y, self.tile_size, self.tile_size))
+                #pygame.draw.rect(self.win, color, (screen_x, screen_y, self.tile_size, self.tile_size))
+                self.win.blit(self.tile_images[biome], (screen_x, screen_y))
 
                 player_screen_x = (player["x"] - cam_left) * self.tile_size
                 player_screen_y = (player["y"] - cam_top) * self.tile_size
-                pygame.draw.rect(self.win,player_,
-                                 (player_screen_x, player_screen_y, self.tile_size, self.tile_size))
+                #pygame.draw.rect(self.win,player_,
+                                 #(player_screen_x, player_screen_y, self.tile_size, self.tile_size))
+                self.win.blit(self.player_image, (player_screen_x, player_screen_y))
+
 
         pygame.display.flip()
         #pygame.display.update()
